ffefb5b6b47b3f8d66ee7f687af1a835f0d38001
Pengyu Nie <prodigy.sov@gmail.com>
2018-07-24 17:49:49 -0500

    Including the installation scripts.

--- pyutil/__init__.py
+++ pyutil/__init__.py
@@ -4,3 +4,6 @@ import sys
 module_root = os.path.dirname(os.path.realpath(__file__)) + "/.."
 if module_root not in sys.path:
     sys.path.insert(0, module_root)
+
+
+name = "pyutil"
---
+++ setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="pyutil",
+    version="0.0.1",
+    author="Pengyu Nie",
+    author_email="prodigy.sov@gmail.com",
+    description="Python utilities",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prodigysov/pyutil",
+    packages=setuptools.find_packages(),
+    classifiers=(
+        "Programming Language :: Python :: 3.5",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: POSIX :: Linux",
+    ),
+)
