507f3d13f2f1373f577e427d45f208b952ce53ef
Pengyu Nie <prodigy.sov@gmail.com>
2018-07-27 13:57:57 -0500

    Fixing an error in pyutil/__init__

--- pyutil/__init__.py
+++ pyutil/__init__.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-module_root = os.path.dirname(os.path.realpath(__file__)) + "/../.."
+module_root = os.path.dirname(os.path.realpath(__file__)) + "/.."
 if module_root not in sys.path:
     sys.path.insert(0, module_root)
 
