ba70ccb66a856a70613e45775502ef28f329f46a
Pengyu Nie <prodigy.sov@gmail.com>
2018-12-13 17:03:33 -0600

    New: - Added new IO format: json-nosort.

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pyutil",
-    version="0.2.16",
+    version="0.2.17",
     author="Pengyu Nie",
     author_email="prodigy.sov@gmail.com",
     description="Python utilities",
